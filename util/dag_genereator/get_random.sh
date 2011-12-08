68992e5478d1c840b113ba6db2c1cda817900aff
Andre Esteve <andreesteve@gmail.com>
2011-12-08 14:27:16 -0200

    Random graphs have now less nodes. Change random script

--- util/dag_genereator/get_random.sh
+++ util/dag_genereator/get_random.sh
@@ -1,4 +1,4 @@
 #Gera random dags com tarefas com no maximo 200 nós,
 #que levam entre 1 min a 60min cada e geram entre 10kB e 10MB de dados
 
-./run.sh 1 50 200 60 1200 10000 10000000 0
+./run.sh 1 20 100 60 1200 10000 10000000 0
